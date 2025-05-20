c991eac76585463c9c6b0342db96a6dcd686a203
Ethan Stratton <e-strattank@outlook.com>
2025-05-20 15:16:32 -0600

    GameDao and MemoryGameDao have necessary methods

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -5,9 +5,11 @@ import java.util.HashSet;
 import chess.ChessGame;
 
 public interface GameDAO {
-    HashSet<GameData> listGames(String username);
+    HashSet<GameData> listGames();
     void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game);
     GameData getGame(int gameID) throws DataAccessException;
     void createGame(GameData game);
+    void updateGame(GameData game);
     void clear();
+    boolean checkGameExists(int gameID);
 }
--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -3,7 +3,6 @@ package dataAccess;
 import chess.ChessGame;
 import model.GameData;
 
-import javax.swing.text.SimpleAttributeSet;
 import java.util.HashSet;
 
 public class MemoryGameDAO implements GameDAO {
@@ -14,17 +13,30 @@ public class MemoryGameDAO implements GameDAO {
         db = HashSet.newHashSet(16);
     }
 
+    public HashSet<GameData> listGames() {
+        return db;
+    }
+
     @Override
-    public HashSet<GameData> listGames(String username) {
-        HashSet<GameData> games = HashSet.newHashSet(16);
+    public boolean checkGameExists(int gameID) {
         for (GameData game : db) {
-            if (game.whiteUsername().equals(username) ||
-                    game.blackUsername().equals(username)) {
-                games.add(game);
+            if (game.gameID() == gameID) {
+                return true;
             }
         }
-        return games;
+        return false;
     }
+
+    @Override
+    public void updateGame(GameData game) {
+        try {
+            db.remove(getGame(game.gameID()));
+            db.add(game);
+        } catch (DataAccessException e) {
+            db.add(game);
+        }
+    }
+
 
     @Override
     public void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
